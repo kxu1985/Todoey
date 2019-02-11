7332239d48215dc301d4e7043dff8ea4e763c044
Ke Xu <kxu@Kelly.local>
2019-02-11 11:10:19 -0800

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ke Xu on 2/4/19.
+//  Copyright © 2019 Ke Xu. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
